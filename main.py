d866943319ea468c622d8ed49d30765b8c19b463
mauriziopiu <maurizio.piu427@gmail.com>
2024-05-22 12:19:39 +0200

    style: cleanup python file

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import json
 import os
 
 # Path to your JSON file
-json_file_path = './data/bookmarks-2024-05-22.json'
+json_file_path = './data/<FILENAME>.json'
 
 # Load the JSON data from a file
 with open(json_file_path, 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 # Create an output directory for markdown files
-output_dir = "./out/bookmarks_markdown"
+output_dir = "./out"
 os.makedirs(output_dir, exist_ok=True)
 
 # Function to convert bookmark to markdown
@@ -59,7 +59,10 @@ for bookmark in leaf_bookmarks:
     title = bookmark.get('title', f'No Title')
     ctr += 1
     # Create a valid filename by replacing spaces with underscores and removing special characters
-    filename = ''.join(e for e in title if e.isalnum() or e.isspace()).replace(' ', '_') +"-" + str(ctr) + '.md'
+    filename = ''.join(e for e in title if e.isalnum() or e.isspace()).replace(' ', '_') 
+    if len(filename) == 0:
+        filename = "idk"
+    filename += "-" + str(ctr) + '.md'
     filepath = os.path.join(output_dir, filename)
     
     markdown_content = bookmark_to_markdown(bookmark)
